501c1bf1c592a2ccdb0d0c7f116cc0da324b6cab
siqueira123 <122622906+siqueira123@users.noreply.github.com>
2023-06-28 16:59:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function newImage()
+{
+fabric.image.fromURL('imagenDeAniversario.png', function(Img) {
+    blockImageObject = Img;
+
+    blockImageObject.scaleToWidth(400);
+    blockImageObject.scaleToHeight(400);
+    blockImageObject.set({
+    top:0,
+    left:80
+    });
+   canvas.add(blockImageObject);
+    });
+    
+	
+}
+
+function playSound(){
+	x.play();
+}
